230c2e6b88ade11155421281b14cf2d295b4d73f
Chalanka <chalaji99@gmail.com>
2023-10-07 20:22:55 +0530

    UI sketches

---
+++ app/src/main/java/com/example/ead/ActiveReservations.java
@@ -0,0 +1,14 @@
+package com.example.ead;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActiveReservations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_reservations);
+    }
+}
