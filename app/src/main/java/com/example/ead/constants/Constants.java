37b77c0fbc4e540a26eeee75e77de026aba8e988
Chalanka <chalaji99@gmail.com>
2023-10-16 04:26:26 +0530

    IP Address changed

--- app/src/main/java/com/example/ead/constants/Constants.java
+++ app/src/main/java/com/example/ead/constants/Constants.java
@@ -2,5 +2,5 @@ package com.example.ead.constants;
 
 public class Constants {
     //Base URL for API Calls
-    public static final String BASE_URL = "https://10.0.2.2:44346";
+    public static final String BASE_URL = "https://192.168.1.2:44346";
 }
